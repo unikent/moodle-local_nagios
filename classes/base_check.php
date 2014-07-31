27a33d08d64a89e5c5cbc89aa09facc91bd6f032
Skylar Kelty <S.Kelty@kent.ac.uk>
2014-07-31 10:35:11 +0100

    Add base check

---
+++ classes/base_check.php
@@ -0,0 +1,66 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+namespace local_nagios;
+
+defined('MOODLE_INTERNAL') || die();
+
+/**
+ * A base nagios check.
+ */
+abstract class base_check
+{
+    /** Our current status */
+    protected $status;
+
+    /** Our current set of messages */
+    protected $messages = array();
+
+    /**
+     * Execute the check.
+     */
+    public abstract function execute();
+
+    /**
+     * Set an error.
+     */
+    public function error($message) {
+        $this->status = 1;
+        $this->messages[] = $message;
+    }
+
+    /**
+     * Set an warning.
+     */
+    public function warning($message) {
+        $this->status = $this->status === 1 ? 1 : 2;
+        $this->messages[] = $message;
+    }
+
+    /**
+     * Get the status code.
+     */
+    public function get_status() {
+        return $this->status;
+    }
+
+    /**
+     * Get our messages.
+     */
+    public function get_messages() {
+        return $this->messages;
+    }
+}

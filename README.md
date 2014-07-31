3c910b6fb7a797b6fc0ea27dc813d4e8430df873
Skylar Kelty <S.Kelty@kent.ac.uk>
2014-07-31 08:52:30 +0100

    Add base plugin

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 moodle-local_nagios
 ===================
 
-Nagios plugin for Moodle NRPE checks
+Nagios plugin for Moodle NRPE checks.
+
+This plugin aggregates NRPE checks from other plugins and allows administrators to configure individual checks through a web interface.
---
+++ db/access.php
@@ -0,0 +1,33 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Capability definitions.
+ *
+ * @package    local_nagios
+ * @author     Skylar Kelty <S.Kelty@kent.ac.uk>
+ * @copyright  2014 University of Kent
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+$capabilities = array(
+    'mod/nagios:configure' => array(
+        'riskbitmask' => RISK_CONFIG,
+        'captype' => 'write',
+        'contextlevel' => CONTEXT_SYSTEM,
+        'archetypes' => array()
+    )
+);
